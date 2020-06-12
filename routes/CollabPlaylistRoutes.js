0c1547300c1418e84dd12d6997c9f1f08a2055f1
Dustin Cheung <dustincheung@Dustins-MacBook-Pro.local>
2020-06-12 02:21:19 -0400

    added rest update route, called when editing a collabPlaylist

--- routes/CollabPlaylistRoutes.js
+++ routes/CollabPlaylistRoutes.js
@@ -11,14 +11,14 @@ const router = express.Router();
 //				  COLLABPLAYLISTS ROUTES
 //************************************************
 
+//NOTE: b/c we are using SPA the new, edit, show pages do not need to be requeted but rather 
+//the paths are hooked up using react router in the frontend
+
 //INDEX ROUTE:
 router.get("/collabplaylists", async(req, res) => {
 	const collabPlaylists = await CollabPlaylist.find();
 	res.send(collabPlaylists);
 });
-
-//SHOW ROUTE:
-
 
 //CREATE ROUTE: creates collabPlaylist from values in req.body obj and adds to mongo db
 //responds with collabPlaylist obj back to allow dispatch to update redux store
@@ -40,9 +40,20 @@ router.post("/collabplaylists", async(req, res) => {
 
 });
 
-//EDIT ROUTE:
-
 //UPDATE ROUTE:
+router.patch("/collabplaylists/:id", async(req, res) => {
+	const id = req.params.id;
+	const name = req.body.name;
+	const description = req.body.description;
+
+	await CollabPlaylist.findByIdAndUpdate(id, {name: name, description: description}, (err, playlist) => {
+		if(err){
+			console.log(err);
+		}else{
+			res.send(playlist);
+		}
+	})
+})
 
 //DELETE ROUTE: takes id from url param and removes document with that id
 router.delete("/collabplaylists/:id/delete", async(req, res) => {
